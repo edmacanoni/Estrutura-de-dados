acfe24d4de54bfa3687e1dd67eb2051061514bc3
edmacanoni <50583072+edmacanoni@users.noreply.github.com>
2021-04-21 04:52:52 -0300

    Update pilha.cpp

--- pilha.cpp
+++ pilha.cpp
@@ -6,5 +6,51 @@ using namespace std;
 pilha::pilha(){
    tamanho = 0;
    estrutura = new TipoItem[max_itens];
+}
+
+pilha::~pilha(){
+
+delete [] estrutura;
+}
+
+bool pilha::pilhacheia(){
+   return (tamanho == max_itens);
+}
+
+bool pilha::pilhavazia(){
+  return (tamanho == 0);
 
 }
+
+void pilha::inserir(TipoItem item){
+   if(pilhacheia()){
+   cout << "A Pilha esta cheia\n";
+   }else {
+      estrutura[tamanho] = item;
+      tamanho++;
+   }
+}
+
+ TipoItem pilha::remover(){
+   if (pilhavazia()){
+   cout << "vazio";
+   return 0;
+   }else {
+      tamanho--;
+      return estrutura[tamanho]; 
+      }
+    
+ }
+   
+ void pilha::imprimir(){
+    cout << "Pilha: [ ";
+    for (int i=0; i<tamanho; i++){
+    cout << estrutura[i] << " ";
+    }
+    cout << "]\n";
+   
+    
+ int pilha::qualtamanho(){
+ return tamanho;
+ }
+    
